97c47fcaaa6f0f56320c015d41c60a89aa2af9ca
Purity <lusekamakatiani@gmail.com>
2019-09-24 18:00:34 +0300

    Joyce: overview page

--- version2/src/KBCM/src/pages/page-2.js
+++ version2/src/KBCM/src/pages/page-2.js
@@ -1,16 +1,68 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const SecondPage = () => (
+
+const Overview = () => (
   <Layout>
-    <SEO title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <SEO title="Overview" />
+    <section className="Banner">
+          <div className="overlay"></div>
+
+      
+            <div className="banner_message">
+             
+               <h1>
+              Overview
+              
+          
+              </h1>
+             
+           
+
+            </div>
+  
+         
+        
+        </section>
+
+        <section id="overview" className="overview-one">
+          <div className="inner">
+
+            <div className="row">
+              <div className="col-md-12 col-sm-12 col-12">
+                <div>
+                <h2> Host Your Own</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+
+            <div className="row">
+              <div className="col-md-12 col-sm-12 col-12">
+                <div>
+                <h2> How it Helps</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+            
+            <div className="row">
+              <div className="col-md-12 col-sm-12 col-12">
+                <div>
+                <h2> KBCM agenda</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+          
+          </div>
+
+        </section>
+
+   
   </Layout>
 )
 
-export default SecondPage
+export default Overview
