79abd39f45bff397320695f8d6917ee20ec47e34
Purity <lusekamakatiani@gmail.com>
2019-09-24 20:54:53 +0300

    Joyce: ideas page structure

---
+++ version2/src/KBCM/src/pages/ideas.js
@@ -0,0 +1,102 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import styles from '../components/ideas.css'
+
+const Ideas = () => (
+  <Layout>
+    <SEO title="Ideas" />
+    <section className="Banner">
+          <div className="overlay"></div>
+
+      
+            <div className="banner_message">
+             
+               <h1>
+              Ideas
+              
+          
+              </h1>
+             
+           
+
+            </div>
+  
+         
+        
+        </section>
+
+        <section id="ideas" className="ideas-one">
+          <div className="">
+          <div className="bakesale">
+            <div className="inner">
+            <div className="row  ">
+              <div className="col-md-6 col-sm-12 col-12 offset-md-6 ">
+                <div>
+                <h2>01</h2>
+                <h2>Have a bake sale at work/school/church</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+            </div>
+          </div>
+           
+        <div className="residential">
+          <div className="inner">
+            <div className="row ">
+              <div className="col-md-6 col-sm-12 col-12">
+                <div>
+                <h2>02</h2>
+                <h2> Host a Coffee Morning as a residential estate</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="pledges">
+          <div className="inner">
+            <div className="row">
+                <div className="col-md-6 col-sm-12 col-12 offset-md-6">
+                  <div>
+                  <h2>03</h2>
+                  <h2> Get staff members to pledge in advance</h2>
+                  <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                  </div>
+                </div>
+              </div>
+          </div>
+        </div>
+
+        <div className="chama">
+          <div className="inner">
+          <div className="row">
+              <div className="col-md-6 col-sm-12 col-12 ">
+                <div>
+                <h2>04</h2>
+                <h2> Have an office/school auction</h2>
+                <p>as it is sometimes known, is dummy text used in laying out print, graphic or web designs. The passage is attributed to an unknown typesetter in the 15th century who is thought to have scrambled parts of Cicero's De Finibus Bonorum et Malorum for use in a type specimen book. </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+  
+
+            
+            
+
+          
+          </div>
+
+        </section>
+
+   
+  </Layout>
+)
+
+export default Ideas
