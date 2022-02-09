93d3fd1f1ffe734a21c267f92dfacefcedb7a16a
NgatiaJeffers <jeffersongakuya@gmail.com>
2022-02-09 18:48:14 +0300

    JG:Add gallery page and get current year on the footer

---
+++ version2/src/KBCM/src/pages/gallery.js
@@ -0,0 +1,20 @@
+import React from "react";
+import Layout from "../components/layout";
+import SEO from "../components/seo";
+
+class gallery extends React.Component {
+    render() {
+        return (
+            <Layout>
+                {/* <SEO title="Gallery"> */}
+
+                <div className="mt-8">
+                    Hello World!
+                </div>
+            </Layout>
+        )
+    }
+
+}
+
+export default gallery;
