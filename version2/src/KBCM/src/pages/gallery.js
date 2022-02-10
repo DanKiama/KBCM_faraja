00c29088c22d7bca0318e85d4395a3aea78f1af9
NgatiaJeffers <jeffersongakuya@gmail.com>
2022-02-10 14:30:58 +0300

    JG:Add gallery section with images

--- version2/src/KBCM/src/pages/gallery.js
+++ version2/src/KBCM/src/pages/gallery.js
@@ -2,114 +2,158 @@ import React from "react";
 import Img from 'gatsby-image';
 import get from 'lodash/get';
 import { Link, graphql } from 'gatsby';
-import baguetteBox from "baguettebox.js";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+// import baguetteBox from "baguettebox.js";
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
 class gallery extends React.Component {
     render() {
-        baguetteBox.run('.gallery');
+        // baguetteBox.run('.gallery');
         return (
             <Layout>
                 <SEO title="Gallery" />
 
-                <section className="gallery-section-hero bg-image">
-                    <div className="row justify-content-center align-items-center">
-                        <div class="col-md-7 text-center">
-                            <h1 class="text-white heading text-uppercase aos-init aos-animate" data-aos="fade-up">Memories in Snap</h1>
-                            <p class="lead text-white mb-5 aos-init" data-aos="fade-up" data-aos-delay="100">If a picture paints a thousand words, then a let a picture inspire a thousand words.</p>
+                <div className="gallery-section-hero bg-image">
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12 text-center">
+                                <div className="hero-text">
+                                    {/* <h2>Memories in a Snap</h2> */}
+                                </div>
+                            </div>
+                        </div >
+                    </div >
+                </div >
+
+                {/* Gallery Section */}
+                <section section className="gallery items" >
+                    <div className="container">
+                        <div className="row">
+                            <div className="col-lg-12">
+                                <ul className="gallery_filter">
+                                    <li className="active" data-filter="*">All</li>
+                                    <li data-filter=".coffee-morning">Coffee Morning</li>
+                                    <li data-filter=".bike-ride">Bike Ride</li>
+                                    <li data-filter=".meeting">Meeting</li>
+                                    <li data-filter=".exercise-center">Exercise Center</li>
+                                </ul>
+                            </div>
                         </div>
-                    </div>
-                </section>
 
-                {/* Gallery Body */}
-                <div className="container-fluid">
-                    <section className="row align-items-stretch photos">
-                        <div className="col-12 ">
-                            <div className="gallery row align-items-stretch">
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img1.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img2.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img3.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img4.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img5.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
+                        <div className="row gallery_image">
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
                                         <img src="../img6.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                                        <a className="view-btn" href="../img6.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../img7.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
+                                        <img src="../img1.jpg" alt="image" className="img-fluid" />
+                                        <a className="view-btn" href="../img1.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic4.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
+                                        <img src="../img2.jpg" alt="image" className="img-fluid" />
+                                        <a className="view-btn" href="../img2.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic5.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
+                                        <img src="../img4.jpg" alt="image" className="img-fluid" />
+                                        <a className="view-btn" href="../img4.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div className="col-6 col-md-6 col-lg-4 aos-init aos-animate">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic6.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
+                                        <img src="../img3.jpg" alt="image" className="img-fluid" />
+                                        <a className="view-btn" href="../img3.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
+                            </div>
 
-                                <div className="col-6 col-md-6 col-lg-4">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic7.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic8.jpg" alt="image" className="img-fluid" />
-                                    </a>
-                                </div>
-
-                                <div className="col-6 col-md-6 col-lg-4">
-                                    <a className="lightbox d-block photo-item" href="../img.jpg">
-                                        <img src="../pic9.jpg" alt="image" className="img-fluid" />
-                                    </a>
+                            <div className="col-lg-4 col-md-6 col-sm-6 coffe-morning">
+                                <div className="gallery_item">
+                                    <div className="gallery_item_image set-bg">
+                                        <img src="../img5.jpg" alt="image" className="img-fluid" />
+                                        <a className="view-btn" href="../img5.jpg">
+                                            <i className="fa-solid fa-magnifying-glass"></i>
+                                            <FontAwesomeIcon icon="fa-solid fa-magnifying-glass" />
+                                        </a>
+                                    </div>
+                                    <div className="gallery_item_text">
+                                        <h4>Coffee Mornings</h4>
+                                        <ul>
+                                            <li>Coffee & snack</li>
+                                        </ul>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </section>
-                </div>
-            </Layout>
+                    </div>
+                </section>
+            </Layout >
         )
     }
 
