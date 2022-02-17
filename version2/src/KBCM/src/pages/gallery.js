2906ce7cc138cb6a620a107ccd6482ca906688b5
NgatiaJeffers <jeffersongakuya@gmail.com>
2022-02-17 14:47:09 +0300

    JG:Add Carousel to the images

--- version2/src/KBCM/src/pages/gallery.js
+++ version2/src/KBCM/src/pages/gallery.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { graphql } from 'gatsby';
-import StackGrid, { easings, transitions } from "react-stack-grid";
+// import StackGrid, { easings, transitions } from "react-stack-grid";
+import Gallery from 'react-grid-gallery';
 
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const { scaleDown, fadeUp } = transitions;
+// const { scaleDown, fadeUp } = transitions;
 
 class gallery extends React.Component {
-    constructor() {
-        super();
-        this.state = { width: -1 };
-    }
-
-
     render() {
         console.log(this.props)
         const gallery = this.props.data.allContentfulPhotoGallery.nodes[0].images
@@ -23,15 +18,17 @@ class gallery extends React.Component {
             let imageData = [];
             imageData = {
                 src: image.photo.file.url,
-                srcSet: image.photo.file.url,
-                sizes: ["(min-width: 480px) 50vw,(min-width: 1024px) 33.3vw,100vw"],
+                srcset: image.photo.file.url,
                 width: image.photo.file.details.image.width,
                 height: image.photo.file.details.image.height,
+                thumbnail: image.photo.file.url,
+                thumbnailWidth: image.photo.file.details.image.width,
+                thumbnailHeight: image.photo.file.details.image.height,
             }
             newImageData.push(imageData);
         })
         console.log(newImageData);
-        const width = this.state.width;
+
         return (
             <Layout>
                 <SEO title="Gallery" />
@@ -43,33 +40,26 @@ class gallery extends React.Component {
                                     <h2>Memories in a Snap</h2>
                                 </div>
                             </div>
-                        </div >
-                    </div >
-                </div >
-
-                {/* Gallery Section */}
-                <section section className="gallery" >
-                    <div>
-                        <StackGrid
-                            columnWidth={400}
-                            appearDelay={10}
-                            duration={480}
-                            easing={easings.quartOut}
-                            appear={fadeUp.appear}
-                            appeared={fadeUp.appeared}
-                            enter={fadeUp.enter}
-                            entered={fadeUp.entered}
-                            leaved={fadeUp.leaved}
-                            monitorImagesLoaded={false}
-                        >
-                            {gallery.map((image, i) => (
-                                <figure key={i} className="image">
-                                    <img src={image.photo.file.url} />
-                                </figure>
-                            ))};
-                        </StackGrid>
+                        </div>
                     </div>
-                </section>
+                </div>
+
+
+                <div className="container">
+                    <div className="row">
+                        <section className="gallery">
+                            <Gallery
+                                className=""
+                                images={newImageData}
+                                rowHeight={180}
+                                enableImageSelection={false}
+                                backdropClosesModal={true}
+                                lightboxWidth={500}
+                            />
+
+                        </section>
+                    </div>
+                </div>
 
 
             </Layout >
@@ -83,7 +73,7 @@ export default gallery;
 
 
 export const query = graphql`
-    query galleryQueryAndGalleryQuery {
+    query GalleryQuery {
         allContentfulPhotoGallery {
             nodes {
                 images {
